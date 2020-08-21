8bd47208f2d3c383b3fcd29123d742fcb4f8d011
Andre Soares <andresoares37@gmail.com>
2020-08-21 16:04:32 -0300

    Organizando o código de acesso em um DAO

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -15,26 +15,42 @@ namespace Alura.Loja.Testes.ConsoleApp
             RecuperarProdutos();
             ExcluirProdutos();
             RecuperarProdutos();
+            AtualizarProdutos();
+        }
+
+        private static void AtualizarProdutos()
+        {
+            //incluir produto
+            GravarUsandoEntity();
+            RecuperarProdutos();
+
+            //alterar produto
+            using (var repo = new ProdutoDAOEntity())
+            {
+                Produto primeiro = repo.Produtos().First();
+                primeiro.Nome = "Harry Potter e a Ordem da Fênix - Editado";
+                repo.Atualizar(primeiro);
+            }
+            RecuperarProdutos();
         }
 
         private static void ExcluirProdutos()
         {
-            using (var repo = new LojaContext())
+            using (var repo = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = repo.Produtos.ToList();
+                IList<Produto> produtos = repo.Produtos();
                 foreach (var item in produtos)
                 {
-                    repo.Produtos.Remove(item);
+                    repo.Remover(item);
                 }
-                repo.SaveChanges();
             }
         }
 
         private static void RecuperarProdutos()
         {
-            using (var repo = new LojaContext())
+            using (var repo = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = repo.Produtos.ToList();
+                IList<Produto> produtos = repo.Produtos();
                 Console.WriteLine("foram encontrados {0} produto(s)", produtos.Count);
                 foreach (var item in produtos)
                 {
@@ -61,28 +77,25 @@ namespace Alura.Loja.Testes.ConsoleApp
             p3.Categoria = "Livros";
             p3.Preco = 19.89;
 
-
-
-            using (var contexto = new LojaContext())
+            using (var repo = new ProdutoDAOEntity())
             {
-                contexto.Produtos.Add(p1);
-                contexto.Produtos.Add(p2);
-                contexto.Produtos.Add(p3);
-                contexto.SaveChanges();
+                repo.Adicionar(p1);
+                repo.Adicionar(p2);
+                repo.Adicionar(p3);                
             }
         }
 
-        //private static void GravarUsandoAdoNet()
-        //{
-        //    Produto p = new Produto();
-        //    p.Nome = "Harry Potter e a Ordem da Fênix";
-        //    p.Categoria = "Livros";
-        //    p.Preco = 19.89;
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
 
-        //    using (var repo = new ProdutoDAO())
-        //    {
-        //        repo.Adicionar(p);
-        //    }
-        //}
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
     }
 }
